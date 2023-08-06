7936242cf4d08903b86c12ab93df012b670d8ac5
victorjatoba <victorjatoba10@gmail.com>
2023-08-06 20:38:06 +0100

    add spring war project

---
+++ war-demo/src/main/java/com/example/wardemo/WarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDemoApplication.class, args);
+	}
+
+}
