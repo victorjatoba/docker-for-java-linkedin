5e07cc747b877e9834acbe45392859f281dcc6d7
victorjatoba <victorjatoba10@gmail.com>
2023-08-06 19:14:39 +0100

    first commit

---
+++ springboot3-1-2-war-java-17/src/main/java/com/example/dockerforjavadevs/springboot312warjava17/springboot312warjava17/Springboot312WarJava17Application.java
@@ -0,0 +1,13 @@
+package com.example.dockerforjavadevs.springboot312warjava17.springboot312warjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot312WarJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot312WarJava17Application.class, args);
+	}
+
+}
---
+++ springboot3-1-2-war-java-17/src/test/java/com/example/dockerforjavadevs/springboot312warjava17/springboot312warjava17/Springboot312WarJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerforjavadevs.springboot312warjava17.springboot312warjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot312WarJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
